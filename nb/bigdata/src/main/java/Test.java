9102101adecc439ace03024579853206041dfb98
jazzyshi1 <1091229807@qq.com>
2019-12-27 14:45:02 +0800

    some changes to commit

--- nb/bigdata/src/main/java/Test.java
+++ nb/bigdata/src/main/java/Test.java
@@ -2,6 +2,11 @@
  * Created by jazzyshi on 2019/10/18.
  */
 
+import java.io.InputStream;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.concurrent.*;
@@ -14,7 +19,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @Date 2019/10/18 16:33
  * @Version 1.0
  **/
-public class Test {
+public class Test implements InvocationHandler{
     private static AtomicInteger count = new AtomicInteger();
 
     ConcurrentLinkedQueue aeea = new ConcurrentLinkedQueue<Integer>();
@@ -23,10 +28,27 @@ public class Test {
 
     ConcurrentHashMap aa = new ConcurrentHashMap(10);
     Hashtable<String,String> a = new Hashtable(10);
-    public static void main(String[] args) {
-        count.addAndGet(1);
+
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+       // System.out.println(Test.class.getPackage().getName());
+       // count.addAndGet(1);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
+        try {
+            System.out.println(sdf.parse("2019"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    List<String> bb = new CopyOnWriteArrayList<>();
+    public String good(){
+return null;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
 
 }
