0d1232ce4f9cbe23f5ddbc347813a4339e29a1fc
jazzyshi1 <1091229807@qq.com>
2020-01-03 10:22:08 +0800

    compact

--- nb/bigdata/src/main/java/Test.java
+++ nb/bigdata/src/main/java/Test.java
@@ -9,8 +9,12 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Date;
+
+import java.lang.Thread;
 
 /**
  * @ClassName Test
@@ -19,36 +23,10 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @Date 2019/10/18 16:33
  * @Version 1.0
  **/
-public class Test implements InvocationHandler{
-    private static AtomicInteger count = new AtomicInteger();
+public class Test {
 
-    ConcurrentLinkedQueue aeea = new ConcurrentLinkedQueue<Integer>();
-    LinkedBlockingDeque<String> cc = new LinkedBlockingDeque<>();
-    ArrayBlockingQueue aaaa = new ArrayBlockingQueue<String>(10);
-
-    ConcurrentHashMap aa = new ConcurrentHashMap(10);
-    Hashtable<String,String> a = new Hashtable(10);
-
-    public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
-       // System.out.println(Test.class.getPackage().getName());
-       // count.addAndGet(1);
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
-        try {
-            System.out.println(sdf.parse("2019"));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public String good(){
-return null;
-    }
-
-    @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        return null;
+    public static void main(String[] args) {
+        new Thread(() -> System.out.println("测试")).start();
     }
 
 }
